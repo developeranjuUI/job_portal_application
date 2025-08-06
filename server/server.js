322e9735b67677dd5e90478d40ec753e5ef66f8f
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-08-06 12:15:38 +0000

    env updated and database updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,11 @@ await connectDB();
 await connectCloudinary();
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  // origin: "*",
+  // methods: ["GET", "POST", "PUT", "DELETE"],
+  // credentials: true
+}));
 app.use(express.json());
 app.use(clerkMiddleware());
 
@@ -42,8 +46,8 @@ const PORT = process.env.PORT || 5000
 
 Sentry.setupExpressErrorHandler(app);
 
-// app.listen(PORT,()=>{
-//     console.log(`server is running on ${PORT}`)
-// })
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+})
 
 export default app;
